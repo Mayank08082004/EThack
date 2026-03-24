b3f2d387f57df8591a31f04f34ad53bb59b8a5df
Mayank08082004 <mayankc1533@gmail.com>
2026-03-24 19:50:13 +0530

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.db.database import SessionLocal
 from sqlalchemy import text
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for now allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
